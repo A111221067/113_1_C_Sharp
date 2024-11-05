bf5737ba5a101d951a14bf3957b72ee7fdaaf683
A111221067 <a111221067@mail.shu.edu.tw>
2024-11-05 17:50:26 +0800

    113-11-05

---
+++ 113-11-05/1/1/Form1.Designer.cs
@@ -0,0 +1,174 @@
+﻿namespace _1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            groupBox1 = new GroupBox();
+            radioButton5 = new RadioButton();
+            radioButton6 = new RadioButton();
+            radioButton7 = new RadioButton();
+            radioButton8 = new RadioButton();
+            radioButton1 = new RadioButton();
+            groupBox2 = new GroupBox();
+            radioButton2 = new RadioButton();
+            radioButton3 = new RadioButton();
+            groupBox1.SuspendLayout();
+            groupBox2.SuspendLayout();
+            SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(radioButton5);
+            groupBox1.Controls.Add(radioButton6);
+            groupBox1.Controls.Add(radioButton7);
+            groupBox1.Controls.Add(radioButton8);
+            groupBox1.Location = new Point(37, 39);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(314, 273);
+            groupBox1.TabIndex = 4;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "groupBox1";
+            // 
+            // radioButton5
+            // 
+            radioButton5.AutoSize = true;
+            radioButton5.Location = new Point(108, 185);
+            radioButton5.Name = "radioButton5";
+            radioButton5.Size = new Size(99, 19);
+            radioButton5.TabIndex = 7;
+            radioButton5.TabStop = true;
+            radioButton5.Text = "radioButton5";
+            radioButton5.UseVisualStyleBackColor = true;
+            // 
+            // radioButton6
+            // 
+            radioButton6.AutoSize = true;
+            radioButton6.Location = new Point(108, 148);
+            radioButton6.Name = "radioButton6";
+            radioButton6.Size = new Size(99, 19);
+            radioButton6.TabIndex = 6;
+            radioButton6.TabStop = true;
+            radioButton6.Text = "radioButton6";
+            radioButton6.UseVisualStyleBackColor = true;
+            // 
+            // radioButton7
+            // 
+            radioButton7.AutoSize = true;
+            radioButton7.Location = new Point(108, 108);
+            radioButton7.Name = "radioButton7";
+            radioButton7.Size = new Size(99, 19);
+            radioButton7.TabIndex = 5;
+            radioButton7.TabStop = true;
+            radioButton7.Text = "radioButton7";
+            radioButton7.UseVisualStyleBackColor = true;
+            // 
+            // radioButton8
+            // 
+            radioButton8.AutoSize = true;
+            radioButton8.Location = new Point(108, 68);
+            radioButton8.Name = "radioButton8";
+            radioButton8.Size = new Size(99, 19);
+            radioButton8.TabIndex = 4;
+            radioButton8.TabStop = true;
+            radioButton8.Text = "radioButton8";
+            radioButton8.UseVisualStyleBackColor = true;
+            // 
+            // radioButton1
+            // 
+            radioButton1.AutoSize = true;
+            radioButton1.Location = new Point(26, 54);
+            radioButton1.Name = "radioButton1";
+            radioButton1.Size = new Size(99, 19);
+            radioButton1.TabIndex = 0;
+            radioButton1.TabStop = true;
+            radioButton1.Text = "radioButton1";
+            radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // groupBox2
+            // 
+            groupBox2.Controls.Add(radioButton3);
+            groupBox2.Controls.Add(radioButton2);
+            groupBox2.Controls.Add(radioButton1);
+            groupBox2.Location = new Point(390, 53);
+            groupBox2.Name = "groupBox2";
+            groupBox2.Size = new Size(160, 259);
+            groupBox2.TabIndex = 5;
+            groupBox2.TabStop = false;
+            groupBox2.Text = "groupBox2";
+            // 
+            // radioButton2
+            // 
+            radioButton2.AutoSize = true;
+            radioButton2.Location = new Point(26, 119);
+            radioButton2.Name = "radioButton2";
+            radioButton2.Size = new Size(99, 19);
+            radioButton2.TabIndex = 1;
+            radioButton2.TabStop = true;
+            radioButton2.Text = "radioButton2";
+            radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            radioButton3.AutoSize = true;
+            radioButton3.Location = new Point(26, 171);
+            radioButton3.Name = "radioButton3";
+            radioButton3.Size = new Size(99, 19);
+            radioButton3.TabIndex = 2;
+            radioButton3.TabStop = true;
+            radioButton3.Text = "radioButton3";
+            radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(groupBox2);
+            Controls.Add(groupBox1);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            groupBox2.ResumeLayout(false);
+            groupBox2.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+        private GroupBox groupBox1;
+        private RadioButton radioButton5;
+        private RadioButton radioButton6;
+        private RadioButton radioButton7;
+        private RadioButton radioButton8;
+        private RadioButton radioButton1;
+        private GroupBox groupBox2;
+        private RadioButton radioButton3;
+        private RadioButton radioButton2;
+    }
+}
---
+++ 113-11-05/Chapter 04/Tutorial 4-4/Fuel Economy with TryParse/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics.Eventing.Reader;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Fuel_Economy_with_TryParse
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void calculateButton_Click(object sender, EventArgs e)
+        {
+            double Kms;
+            double liters;
+            if (double.TryParse(milesTextBox.Text.out Kms));
+            {
+                if (double.TryParse(gallonsTextBox.Text.out liters));
+            }
+               else;
+            {
+                MessageBox.Show("xxxxxxxxxxx");
+            }
+         else;
+          {
+                MessageBox.Show("xxxxxxxxxxx");
+            }
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            // Close the form.
+            this.Close();
+        }
+    }
+}
